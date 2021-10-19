6fe7e9ce56fee8575cb2e122a890606e58139cce
Nicolas Cano <nicolascano40@hotmail.com>
2021-10-18 23:30:38 -0300

    Agregar multiplicacion y division

---
+++ funciones.js
@@ -0,0 +1,17 @@
+function multiplicar(num1, num2) {
+  const resultado = num1 * num2;
+  if (num1 === 0 || num2 === 0) {
+    return 0;
+  } else {
+    return resultado;
+  }
+}
+
+function division(dividendo, divisor) {
+  const resultado = dividendo / divisor;
+  if (divisor === 0) {
+    return "No se puede dividir por cero";
+  } else {
+    return resultado;
+  }
+}
